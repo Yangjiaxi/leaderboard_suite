445b0735e32e0cf136d9a9ddaa7d2f13f9114efb
Yangjiaxi <jason.yang98@foxmail.com>
2023-02-27 10:40:50 +0800

    <MOD> test 2

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,10 +2,6 @@ import * as React from 'react';
 import { memo } from "react";
 import MainFrame from './MainFrame';
 
-const App = memo(({ header, detail, schema, data }) => {
-    return (
-        <MainFrame header={header} detail={detail} schema={schema} data={data} />
-    );
-});
+const App = memo(({ header, detail, schema, data }) => <MainFrame header={header} detail={detail} schema={schema} data={data} />);
 
 export default App;
